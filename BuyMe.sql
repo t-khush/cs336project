b848f8222d690e48a8e4289d1eeb34d29e96ef2f
Prathik Lolla <prathik.lolla@gmail.com>
2022-04-16 15:12:20 -0400

    pushing buyme.sql

---
+++ BuyMe.sql
@@ -0,0 +1,8 @@
+#CREATE DATABASE  IF NOT EXISTS `BuyMe` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `BuyMe`;
+
+#create table user (username varchar(30), password varchar(30));
+#insert into user (username, password) values ("plolla", "hello");
+
+select * from user;
+
